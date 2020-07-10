5641603e752174af284d0d6876f67f5343011d9f
Walter Junior <wpmj1277@gmail.com>
2020-07-10 20:59:28 -0300

    Capture's unit tests

--- pkg/integrations/pagseg/pagseg_test.go
+++ pkg/integrations/pagseg/pagseg_test.go
@@ -4,30 +4,37 @@ import (
 	"testing"
 )
 
-var chargeRequest = ChargeRequest{
-	ReferenceID: "76c35c0b-34d5-4ecc-af9d-3b2cecab033c",
-	Description: "Testing",
-	Amount: amount{
-		Value:    1000,
-		Currency: "BRL",
-	},
-	PaymentMethod: paymethod{
-		Type:         "CREDIT_CARD",
-		Installments: 1,
-		Capture:      true,
-		Card: card{
-			Number:       "4111111111111111",
-			ExpMonth:     "03",
-			ExpYear:      "2026",
-			SecurityCode: "123",
-			Holder: struct {
-				Name string `json:"name,omitempty"`
-			}{
-				Name: "Jose da Silva",
+var (
+	chargeRequest = ChargeRequest{
+		ReferenceID: "76c35c0b-34d5-4ecc-af9d-3b2cecab033c",
+		Description: "Testing",
+		Amount: amount{
+			Value:    1000,
+			Currency: "BRL",
+		},
+		PaymentMethod: paymethod{
+			Type:         "CREDIT_CARD",
+			Installments: 1,
+			Capture:      true,
+			Card: card{
+				Number:       "4111111111111111",
+				ExpMonth:     "03",
+				ExpYear:      "2026",
+				SecurityCode: "123",
+				Holder: struct {
+					Name string `json:"name,omitempty"`
+				}{
+					Name: "Jose da Silva",
+				},
 			},
 		},
-	},
-}
+	}
+
+	amountRequest = AmountRequest{Amount: amount{Value: 1000}}
+
+	statusPaid = "PAID"
+	successMessage = "SUCESSO"
+)
 
 func TestCreateCharge(t *testing.T) {
 	paymentHelper := func() *ChargeResponse {
@@ -49,17 +56,49 @@ func TestCreateCharge(t *testing.T) {
 
 	t.Run("Assert Status", func(t *testing.T) {
 		response := paymentHelper()
-		expectedStatus := "PAID"
-		if response.Status != expectedStatus {
-			t.Errorf("It was expected '%v' and got '%v'", expectedStatus, response.Status)
+		if response.Status != statusPaid {
+			t.Errorf("It was expected '%v' and got '%v'", statusPaid, response.Status)
 		}
 	})
 
 	t.Run("Assert Message", func(t *testing.T) {
 		response := paymentHelper()
-		expectedMessage := "SUCESSO"
-		if response.PaymentResponse.Message != expectedMessage {
-			t.Errorf("It was expected '%v' and got '%v'", expectedMessage, response.PaymentResponse.Message)
+		if response.PaymentResponse.Message != successMessage {
+			t.Errorf("It was expected '%v' and got '%v'", successMessage, response.PaymentResponse.Message)
+		}
+	})
+}
+
+func TestCapture(t *testing.T) {
+	id := "CHAR_D32A01A9-92A6-4755-B21D-7B6A1291F7AD"
+	paymentHelper := func() *ChargeResponse {
+		pagseg := NewPagSeguro(&HttpRequesterMock{})
+
+		response, err := pagseg.Capture(id, amountRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		return response
+	}
+
+	t.Run("Assert Id", func(t *testing.T) {
+		response := paymentHelper()
+		if response.ID != id {
+			t.Errorf("It was expected '%v' and got '%v'", id, response.ID)
+		}
+	})
+
+	t.Run("Assert Status", func(t *testing.T) {
+		response := paymentHelper()
+		if response.Status != statusPaid {
+			t.Errorf("It was expected '%v' and got '%v'", statusPaid, response.Status)
+		}
+	})
+
+	t.Run("Assert Message", func(t *testing.T) {
+		response := paymentHelper()
+		if response.PaymentResponse.Message != successMessage {
+			t.Errorf("It was expected '%v' and got '%v'", successMessage, response.PaymentResponse.Message)
 		}
 	})
 }
